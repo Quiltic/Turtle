f4d038e8c47353947790843f112bf7ad1dc26816
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2019-08-14 16:14:06 -0400

    Update TurtleUpdate.py

--- TurtleUpdate.py
+++ TurtleUpdate.py
@@ -18,7 +18,7 @@ os.system(("cd "+ cwd))
 
 #reopen turtle
 time.sleep(10)
-callfreind = "python3 " + cwd + "/Turtle.py"
+callfreind = "python3 Turtle.py"
 print(callfreind)
 subprocess.Popen(callfreind)
 print("Updated!")
