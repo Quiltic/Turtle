f48b131f8bb4017257bfcc415f7304511655411b
Quiltic <jbot237@gmail.com>
2019-08-14 14:48:24 -0400

    sigh

--- TurtleUpdate.py
+++ TurtleUpdate.py
@@ -18,7 +18,7 @@ os.system(("cd "+ cwd))
 
 #reopen turtle
 time.sleep(10)
-callfreind = "python " + cwd + "\\Turtle.py"
+callfreind = "python " + cwd + "/Turtle.py"
 print(callfreind)
 subprocess.Popen(callfreind)
 print("Updated!")
