ac649dc090466255319eb9d2af00b32b3427d83a
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2019-08-14 14:11:48 -0400

    Fixed TurtleUpdate?

--- TurtleUpdate.py
+++ TurtleUpdate.py
@@ -1,23 +1,22 @@
-print('hello!')
-"""
 import os, subprocess, time
 
 #get home and where to clone to
 cwd = os.getcwd()
-#mainfile = cwd[:cwd.rfind('\\')]
+mainfile = cwd[:cwd.rfind('\\')]
 print(cwd)
+print(mainfile)
 
 #Clone to location
-os.system(("cd "+ cwd))
+os.system(("cd " + cwd))
 os.system("git pull https://github.com/Quiltic/Turtle.git")
 print("cloned")
 
 #give it a moment or 12 then go home
-time.sleep(60)
+time.sleep(3)
 os.system(("cd "+ cwd))
 
 #reopen turtle
 callfreind = "python " + cwd + "\\Turtle.py"
+print(callfreind)
 subprocess.Popen(callfreind)
 print("Updated!")
-"""
