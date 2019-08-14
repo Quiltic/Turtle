69f583277b1f4ccdaf4e9f3d38233672ecd79250
Joshua Zack <34798284+Quiltic@users.noreply.github.com>
2019-08-14 10:49:08 -0400

    To make Turtle update itself

---
+++ TurtleUpdate.py
@@ -0,0 +1,21 @@
+import os, subprocess, time
+
+#get home and where to clone to
+cwd = os.getcwd()
+mainfile = cwd[:cwd.rfind('\\')]
+print(cwd)
+print(mainfile)
+
+#Clone to location
+os.system(("cd "+ mainfile))
+os.system("git pull https://github.com/Quiltic/Turtle.git")
+print("cloned")
+
+#give it a moment or 12 then go home
+time.sleep(60)
+os.system(("cd "+ cwd))
+
+#reopen turtle
+callfreind = "python " + cwd + "\\Turtle.py"
+subprocess.Popen(callfreind)
+print("Updated!")
