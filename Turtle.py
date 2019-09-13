173d7456f04d36689b329c9053cbeaca3cbe7358
Quiltic <jbot237@gmail.com>
2019-09-13 17:50:20 -0400

    Minor_change_somewhere

--- Turtle.py
+++ Turtle.py
@@ -160,24 +160,7 @@ def delete_file(file, guild):
 ###################### Light Commands ###################
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-@bot.command()
-async def setcolor(ctx, red = 255, green = 255, blue = 255):
-    """Changes the color to be ____."""
-    if await light_perms(ctx):
-        msg = "Color is now  %s, %s, %s." % (red,green,blue)
-        await connect_lights(red,green,blue)
-
-        await ctx.send(msg)
-
-
-
-
-@bot.command()
-async def color(ctx):
-    """States the color curently being used."""
-    #color = Lights["Color"]
-    msg = "Color is currently %s." % (color)
-    await ctx.send(msg)
+#yep it got moved
 
 
 
